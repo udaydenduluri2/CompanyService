86f1b66cdd6bfb572b5fb96a2615f83487f94256
Kanaka Durga <dkanakadurga@gmail.com>
2018-06-12 09:09:00 +0100

    adding test - UD

--- CompanyService/Controllers/AuthenticationController.cs
+++ CompanyService/Controllers/AuthenticationController.cs
@@ -33,5 +33,21 @@ namespace CompanyService.Controllers
         {
             return Ok();
         }
+
+        [HttpGet]
+        public IEnumerable<Models.SearchResultDto> Search(string searchTerm)
+        {
+            var results = new List<Models.SearchResultDto>
+            {
+                new Models.SearchResultDto{ Name = "Name1", Description = "Description 1", ImageId="Image1", Price= "£12.78" },
+                new Models.SearchResultDto{ Name = "Name2", Description = "Description 2", ImageId="Image2", Price= "£32.98" },
+                new Models.SearchResultDto{ Name = "Name3", Description = "Description 3", ImageId="Image3", Price= "£96.70" },
+                new Models.SearchResultDto{ Name = "Test1", Description = "Test Desc 1", ImageId="TestImage1", Price= "£102.78" },
+                new Models.SearchResultDto{ Name = "Test2", Description = "Test Desc 2", ImageId="TestImage2", Price= "£392.98" },
+                new Models.SearchResultDto{ Name = "Test3", Description = "Test Desc 3", ImageId="TestImage3", Price= "£196.70" }
+            };
+
+            return results.Where(r => r.Name.Contains(searchTerm));
+        }
     }
 }
